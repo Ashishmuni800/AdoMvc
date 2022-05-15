91e94dc14320506bf0fcfa2d4de94a06ce29ce95
Ashish <ashishmunigupta100@gmail.com>
2022-05-15 20:17:57 +0530

    Add project files.

---
+++ AdoMvc/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AdoMvc.Models;
+using AdoMvc.ViewModel;
+
+namespace AdoMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        EmployeeDb db = new EmployeeDb();
+        public ActionResult Index()
+        {
+            var data = db.GetEmployees();
+            return View(data);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee e)
+        {
+            db.CreateList(e);
+            return RedirectToAction("index");
+        }
+        public ActionResult Delete(int id)
+        {
+            db.DeleteList(id);
+            return RedirectToAction("index");
+        }
+        public ActionResult Edit(int id)
+        {
+            var row = db.GetEmployees().Find(model => model.Id == id);
+            return View(row);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, Employee emp)
+        {
+            db.UpdateList(emp);
+            return RedirectToAction("Index");
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
